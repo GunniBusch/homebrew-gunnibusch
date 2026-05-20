baeb2801e17560524bbf500c881e186378abb00b
Leon Adomaitis <47161699+GunniBusch@users.noreply.github.com>
2026-05-20 22:10:20 +0000

    edit: add 0.1.0 bottle.

--- Formula/e/edit.rb
+++ Formula/e/edit.rb
@@ -5,6 +5,13 @@ class Edit < Formula
   sha256 "26aa65f6d9168bf062dd7d0cdf35af15aa9bbecb518a85348c1b3937fbf095b0"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/GunniBusch/homebrew-gunnibusch/releases/download/edit-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "48990abe40501e3616dac1e5b689a61547629c0c0d8ae1839880dc5608132bd1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "05a2d169af5d07ced18574969edfa824ea1f8f80527a4373bce1ba04507c2a51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "310be9e5e4fea34628d214204297c8cf32b6478040fcd05c0e7144119b54afe6"
+  end
+
   depends_on "rust" => :build
 
   def install
